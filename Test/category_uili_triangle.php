a09b4bfcb451bda3e8669d544a502565eb66a3e3
videru <videru@gmail.com>
2016-01-04 03:21:18 +0900

    IE 브라우저별 호환 및 A태그 hover 색 적용

--- Test/category_uili_triangle.php
+++ Test/category_uili_triangle.php
@@ -1,24 +1,26 @@
-<?
-echo'
+<!DOCTYPE HTML><!-- IE9에서 css:hover 셀렉터 적용 위함-->
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="카테고리ulli_triangle.css">
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+	<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js">IE7_PNG_SUFFIX=".png";</script> 
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" type="text/css" href="category_ulli_triangle.css">
 </head>
 
 <div id="maincontent">
 	<div id="categorylist">
 		<ul>
-			<li><div class="category"><a href="">태조</a></div><div class="triangle-down"></div></li>
-			<li><div class="category"><a href="">정종</a></div><div class="triangle-down"></div></li>
-			<li><div class="category"><a href="">태종</a></div><div class="triangle-down"></div></li>
+			<li><div class="category"><a href="">태조</a></div><div class="triangle-down"></div><p class="categorybanner1 categorybannercommon ">태조실록</p></li>
+			<li><div class="category"><a href="">정종</a></div><div class="triangle-down"></div><p class="categorybanner2 categorybannercommon ">정종실록</p></li>
+			<li><div class="category"><a href="">태종</a></div><div class="triangle-down"></div><p class="categorybanner3 categorybannercommon ">태종실록</p></li>
 		</ul>
 	</div>
-	<div id="categorybanner">
-		
-		<p>세종실록</p>
-	</div>
 </div>
-';
+
+
 
 
 
