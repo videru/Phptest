1955accf3e0ae4ce7c6d5f056ccd954b209c3d81
videru <videru@gmail.com>
2016-01-03 14:59:54 +0900

    최초
    첫 등록

---
+++ Test/카데코리uili_triangle.php
@@ -0,0 +1,24 @@
+
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="카테고리ulli_triangle.css">
+</head>
+
+<div id="maincontent">
+	<div id="categorylist">
+		<ul>
+			<li><div class="category"><a href="">태조</a></div><div class="triangle-down"></div></li>
+			<li><div class="category"><a href="">정종</a></div><div class="triangle-down"></div></li>
+			<li><div class="category"><a href="">태종</a></div><div class="triangle-down"></div></li>
+		</ul>
+	</div>
+	<div id="categorybanner">
+		
+		<p>세종실록</p>
+	</div>
+</div>
+
+
+
+
+
