be0713719ce4cf373d43c8f0ccc37de27efb9f6d
videru <videru@gmail.com>
2016-01-04 03:45:15 +0900

    padding, margin 조정중

--- Test/category_uili_triangle.php
+++ Test/category_uili_triangle.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML><!-- IE9에서 css:hover 셀렉터 적용 위함-->
 <head>
 	<meta charset="UTF-8">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<!--[if lt IE 9]>
 	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
@@ -13,9 +14,9 @@
 <div id="maincontent">
 	<div id="categorylist">
 		<ul>
-			<li><div class="category"><a href="">태조</a></div><div class="triangle-down"></div><p class="categorybanner1 categorybannercommon ">태조실록</p></li>
-			<li><div class="category"><a href="">정종</a></div><div class="triangle-down"></div><p class="categorybanner2 categorybannercommon ">정종실록</p></li>
-			<li><div class="category"><a href="">태종</a></div><div class="triangle-down"></div><p class="categorybanner3 categorybannercommon ">태종실록</p></li>
+			<li><div class="category"><a href="">태조</a></div><div class="triangle-down"></div><p class="categorybanner1 categorybannercommon">태조실록</p></li>
+			<li><div class="category"><a href="">정종</a></div><div class="triangle-down"></div><p class="categorybanner2 categorybannercommon">정종실록</p></li>
+			<li><div class="category"><a href="">태종</a></div><div class="triangle-down"></div><p class="categorybanner3 categorybannercommon">태종실록</p></li>
 		</ul>
 	</div>
 </div>
